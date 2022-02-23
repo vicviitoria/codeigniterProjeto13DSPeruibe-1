7c6bc46e0ba303d6c672bf766d90d4e6a739fcf8
jussimar <jussimar@jussimarleal.com.br>
2022-02-23 14:50:59 -0300

    criando novas paginas e editando menu

--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -26,10 +26,10 @@
                         </button>
                         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div class="navbar-nav">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            <a class="nav-link" href="#">Features</a>
-                            <a class="nav-link" href="#">Pricing</a>
-                            <a class="nav-link disabled">Disabled</a>
+                            <a class="nav-link active" aria-current="page" href="home">Home</a>
+                            <a class="nav-link" href="quemsomos">Quem Somos</a>
+                            <a class="nav-link" href="contato">Contato</a>
+                            
                         </div>
                         </div>
                     </div>
