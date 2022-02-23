1272f89b4147da5aba5f2e616be287b2f7274e23
jussimar <jussimar@jussimarleal.com.br>
2022-02-23 14:22:17 -0300

    criando sistema de template

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,14 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('home');
+        echo view('template/header');
+        echo view('home');
+        echo view('template/footer');
+    }
+
+    public function page($page='home'){
+        echo view('template/header');
+        echo view($page);
+        echo view('template/footer');
     }
 }
