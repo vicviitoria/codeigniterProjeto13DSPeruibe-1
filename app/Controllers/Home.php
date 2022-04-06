3611185eb3def283f3dcaa44659f4d7d656a7e7b
jussimar <jussimar@jussimarleal.com.br>
2022-04-06 15:05:27 -0300

    criando controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,8 +2,11 @@
 
 namespace App\Controllers;
 
+use App\Models\PessoasModel;
+
 class Home extends BaseController
 {
+    
     public function index()
     {
         echo view('template/header');
@@ -14,6 +17,19 @@ class Home extends BaseController
     public function page($page='home'){
         echo view('template/header');
         echo view($page);
+        echo view('template/footer');
+    }
+
+    public function pessoa(){
+        $model = new PessoaModel();
+
+        $data = [
+            'title'=>'Pessoas',
+            'pessoas'=>$model->getPessoas()
+        ];
+
+        echo view('template/header');
+        echo view('pessoa',$data);
         echo view('template/footer');
     }
 }
